fd55542fe1a130241a3decb5a3892891db191055
MunSeongyun <borygashill608@gmail.com>
2024-12-05 16:28:02 +0900

    🔨chore: change DB URL to use environment variables for username and password

--- DB/connection.py
+++ DB/connection.py
@@ -1,6 +1,10 @@
 from sqlmodel import Session, create_engine, SQLModel
 from DB import models
+import os
 
-engine = create_engine('mysql+pymysql://root:1234@db/vocabulary', echo=True)
+DB_USERNAME=os.getenv('DB_USERNAME')
+DB_PASSWORD=os.getenv('DB_PASSWORD')
+
+engine = create_engine(f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@db/vocabulary', echo=True)
 
 SQLModel.metadata.create_all(engine)
