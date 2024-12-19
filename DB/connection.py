c997f984c92313859451dd4f774dfa3bc096a12b
MunSeongyun <borygashill608@gmail.com>
2024-12-19 16:29:45 +0900

    🔨refactor: add new env DB_HOST

--- DB/connection.py
+++ DB/connection.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 DB_USERNAME=os.getenv('DB_USERNAME')
 DB_PASSWORD=os.getenv('DB_PASSWORD')
-
-engine = create_engine(f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@db/vocabulary', echo=True)
+DB_HOST=os.getenv('DB_HOST')
+engine = create_engine(f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/vocabulary', echo=True)
 
 SQLModel.metadata.create_all(engine)
